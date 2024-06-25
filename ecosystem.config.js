2470529161601429c37ce7ef54a6f54d9898a61c
youshenlin <mikey8316@gmail.com>
2024-06-25 18:20:46 +0800

    feat: add ecosytem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,16 @@
-exports.apps = [
-    {
-        name: 'aha',
-        script: './app.js',
-        log_date_format: 'YYYY-MM-DD HH:mm Z',
-        max_restarts: 10,
-        instances: '1',
-        autorestart: true,
-        watch: true,
-    }
-];
+module.exports = {
+    apps: [
+        {
+            name: 'aha',
+            script: './app.js',
+            log_date_format: 'YYYY-MM-DD HH:mm Z',
+            interpreter: '/home/ec2-user/.nvm/versions/node/v20.12.2/bin/node',
+            max_restarts: 10,
+            cron_restart: '0 0 * * *',
+            exec_mode: 'fork',
+            instances: '1',
+            autorestart: true,
+            watch: true,
+        },
+    ],
+};
